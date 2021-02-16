032d056753199df17947fb1289371b7065023d45
Danielle <danisteele1984@gmail.com>
2021-02-16 18:24:35 +1100

    React updates from dani branch

--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 2021_02_12_080834) do
   create_table "users", force: :cascade do |t|
     t.text "name"
     t.text "email"
-    t.text "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
