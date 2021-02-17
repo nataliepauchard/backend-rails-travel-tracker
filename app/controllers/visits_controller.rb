37f7042d6a924d26a992153f90adaa28d3044f12
Scott Causby <scottcausby@gmail.com>
2021-02-17 18:19:00 +1100

    updated visits controller

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -1,7 +1,23 @@
 class VisitsController < ApplicationController
+  before_action :check_for_login
   def index
   end
 
   def create
+    country = Country.find_or_create_by :country_code => params[:countryCode], :name => params[:countryName]
+    visit = Visit.create #visit_params to do bucketlist
+    @current_user.visits << visit
+    country.visits << visit
+    render :json => visit
   end
+
+  def check_for_login
+   redirect_to login_path unless @current_user.present?
+  end
+
+  # private
+  # def visit_params
+  #   params.require(:visit).permit(:enter all the things )
+  # end
+
 end
