4272896e4233005eb4c1cb8804ee6255eddb8a21
Jye Harry <jyeharry21@hotmail.com>
2021-02-17 16:51:16 +1100

    added columns to visits

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_16_021729) do
+ActiveRecord::Schema.define(version: 2021_02_17_054236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "conversations", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "countries", force: :cascade do |t|
     t.text "name"
     t.text "continent"
     t.integer "popluation"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.integer "conversation_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -38,6 +51,8 @@ ActiveRecord::Schema.define(version: 2021_02_16_021729) do
     t.integer "country_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "has_visited"
+    t.boolean "on_bucket_list"
   end
 
 end
